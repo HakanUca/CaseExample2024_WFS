e1c3f039c5bb3fa2e7ff088183bfb3aa90bdfe48
HakanUca <hakanhuca@gmail.com>
2024-02-22 01:23:12 +0300

    Health Bar

--- CaseProject_Wfs/Assets/Scripts/OyuncuKontrol.cs
+++ CaseProject_Wfs/Assets/Scripts/OyuncuKontrol.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class OyuncuKontrol : MonoBehaviour
 {
 
     public Transform MermiPos;
     public GameObject mermi;
-    public GameObject patlama
-        ;
+    public GameObject patlama;
+    public Image canImaji;
+    private float canDegeri = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +27,22 @@ public class OyuncuKontrol : MonoBehaviour
             GameObject go = Instantiate(mermi, MermiPos.position, MermiPos.rotation) as GameObject;
             GameObject goPatlama = Instantiate(patlama, MermiPos.position, MermiPos.rotation) as GameObject;
 
-            go.GetComponent<Rigidbody>().velocity = MermiPos.transform.forward * 10f;
+            go.GetComponent<Rigidbody>().velocity = MermiPos.transform.forward * 100f;
             Destroy(go.gameObject, 2f);
             Destroy(goPatlama.gameObject, 2f);
 
+        }
+    }
+
+    private void OnCollisionEnter (Collision c) 
+    {
+        if (c.collider.gameObject.tag.Equals("zombi"))
+        {
+            Debug.Log("Zombi Saldirida.");
+            canDegeri -= 10f;
+            float x = canDegeri / 100f;
+            canImaji.fillAmount = x;
+            canImaji.color = Color.Lerp(Color.red, Color.green, x);
         }
     }
 }
